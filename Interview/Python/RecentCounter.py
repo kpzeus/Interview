39c97036d0dbd1f37792028791524a608547fed0
Prasanna Kumarasamy <kp.zeus@gmail.com>
2023-06-03 20:08:26 +0530

    RecentCounter

---
+++ Interview/Python/RecentCounter.py
@@ -0,0 +1,22 @@
+﻿class RecentCounter(object):
+
+    def __init__(self):
+        self.a = []
+        self.l=0
+
+    def ping(self, t):
+        self.a.append(t)
+        self.l = self.l+1
+        c = 0
+        limit=t-3000
+        for x in self.a:
+            if x < limit:
+                c=c+1
+            else:
+                break
+        
+        while c > 0:
+            self.a.pop(0)
+            self.l = self.l-1
+            c=c-1
+        return self.l
