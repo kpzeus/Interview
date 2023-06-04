696e4474b2a0d0ad9ac923205555b6ad561b82a1
Prasanna Kumarasamy <kp.zeus@gmail.com>
2023-06-04 20:00:21 +0530

    MatrixSumQueries

---
+++ Interview/Python/MatrixSumQueries.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def matrixSumQueries(self, n, queries):
+        rowSeenCount = 0
+        colSeenCount = 0
+        rowSeen = [False] * n
+        colSeen = [False] * n
+        totalSum = 0
+
+        for qi in range(len(queries) - 1, -1, -1):
+            type, index, val = queries[qi]
+            if type == 0 and not rowSeen[index]:
+                rowSeenCount += 1
+                rowSeen[index] = True
+                totalSum += (n - colSeenCount) * val
+
+            if type == 1 and not colSeen[index]:
+                colSeenCount += 1
+                colSeen[index] = True
+                totalSum += (n - rowSeenCount) * val
+
+        return totalSum
---
+++ Interview/Python/Merge.py
@@ -0,0 +1,47 @@
+﻿class Solution(object):
+    def findCircleNum(self, isConnected):
+        i=0
+        j=0
+        d={}
+        n=len(isConnected)
+        m=0
+
+        while i<n:
+            j=0
+            while j<n:
+                if isConnected[i][j] == 1:
+                    if not i in d.keys():
+                        d[i] = set()
+                    if not j in d.keys():
+                        d[j] = set()
+                    d[i].add(j)
+                    for x in list(d[i]):
+                        if not x in d.keys():
+                            d[x] = set()
+                        d[x].add(i)
+                        d[x].add(j)
+                        d[j].add(x)
+                    l = len(d[i])
+                    if l > m:
+                        m=l
+                j+=1
+            i+=1
+
+        
+        if m == n:
+            return 1
+
+        i=0
+        j=0
+        while i < len(d) - 1:
+            j=i+1
+            while j < len(d):
+                if len(d[d.keys()[i]].intersection(d[d.keys()[j]])) > 0:
+                    d[d.keys()[i]] = d[d.keys()[i]].union(d[d.keys()[j]])
+                    del d[d.keys()[j]]
+                    i-=1
+                    break
+                j+=1
+            i+=1
+
+        return len(d)
